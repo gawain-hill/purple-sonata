0128551375f0062fac270356d966390e3ea254e6
Gawain Hill <gawain.hill@googlemail.com>
2024-06-04 15:51:36 +0100

    chore: refactor in preperation for entities and components

--- actions.py
+++ actions.py
@@ -1,40 +1,84 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import Optional, Tuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from engine import Engine
     from entity import Entity
 
 class Action:
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def __init__(self, entity: Entity) -> None:
+        super().__init__()
+        self.entity = entity
+
+    @property
+    def engine(self) -> Engine:
+        """
+        Returns the engine this action is scoped to.
+        """
+        return self.entity.gamemap.engine
+
+    def perform(self) -> None:
         """
         Perform this action with the object needed to determin its scope.
 
-        `engine` is the scope this action is being performed in.
-        `entity` is the object performing the action
         This method must be overridden by Action subclasses
         """
         raise NotImplementedError()
 
 class EscapeAction(Action):
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self) -> None:
          raise SystemExit
+    
+class ActionWithDirection(Action):
+    def __init__(self, entity: Entity, delta_x: int, delta_y: int) -> None:
+        super().__init__(entity)
 
-class MovementAction(Action):
-        def __init__(self, dx: int, dy: int) -> None:
-             super().__init__()
+        self.delta_x = delta_x
+        self.delta_y = delta_y
 
-             self.dx = dx
-             self.dy = dy
+    @property
+    def destination_xy(self) -> Tuple[int, int]:
+        """
+        Returns this actions destination.
+        """
+        return self.entity.x + self.delta_x, self.entity.y + self.delta_y
+    
+    @property
+    def blocking_entity(self) -> Optional[Entity]:
+        """
+        Returns the entity blocking this actions destination
+        """
+        return self.engine.game_map.get_blocking_entity_at_location(*self.destination_xy)
 
-        def perform(self, engine: Engine, entity: Entity) -> None:
-            dst_x = entity.x + self.dx
-            dst_y = entity.y + self.dy
+    def perform(self) -> None:
+        raise NotImplementedError()
+    
+class MeleeAction(ActionWithDirection):
+    def perform(self) -> None:
+        target = self.blocking_entity
+        if not target:
+            return # No entity to attack
+        
+        print(f"You kick the {target.name}, much to its annoyance!")
 
-            if not engine.game_map.in_bounds(dst_x, dst_y):
-                return # Destination is out of bounds.
+class MovementAction(ActionWithDirection):
+    def perform(self) -> None:
+        destination_x, destination_y = self.destination_xy
+
+        if not self.engine.game_map.in_bounds(destination_x, destination_y):
+            return # Destination is out of bounds.
+        
+        if not self.engine.game_map.tiles["walkable"][destination_x, destination_y]:
+            return # Destination not walkable.
+        
+        if self.engine.game_map.get_blocking_entity_at_location(destination_x, destination_y):
+            return # Destination is blocked by an entity.
             
-            if not engine.game_map.tiles["walkable"][dst_x, dst_y]:
-                return # Destination not walkable.
-                
-            entity.move(self.dx, self.dy)
+        self.entity.move(self.delta_x, self.delta_y)
+    
+class BumpAction(ActionWithDirection):
+    def perform(self) -> None:
+        if self.blocking_entity:
+            return MeleeAction(self.entity, self.delta_x, self.delta_y).perform()
+        else:
+            return MovementAction(self.entity, self.delta_x, self.delta_y).perform()
--- game_map.py
+++ game_map.py
@@ -1,21 +1,37 @@
 from __future__ import annotations
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, Optional, TYPE_CHECKING
 
 import numpy as np
 from tcod.console import Console
 import tile_types
 
 if TYPE_CHECKING:
+    from engine import Engine
     from entity import Entity
 
 class GameMap:
-    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()) -> None:
+    def __init__(self, engine: Engine,  width: int, height: int, entities: Iterable[Entity] = ()) -> None:
+        self.engine = engine
         self.width = width
         self.height = height
         self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
+
+    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+        """
+        Test if an entity is blocking the location or not. If an entity is blocking
+        the location then provide it.
+
+        location_x -- the x component of the `game_map` coordinate to test.
+        location_y -- the y component of the `game_map` coordinate to test.
+        """
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+            
+        return None
 
     def in_bounds(self, x: int, y: int) -> bool:
         """
@@ -31,10 +47,10 @@ class GameMap:
         If it isn't, but it's in the "explored" array, then draw it in the "dark" colors.
         Otherwise, the default is "SHROUD".
         """
-        console.rgb[0:self.width, 0:self.height] = np.select(
+        console.rgb[0 : self.width, 0 : self.height] = np.select(
             condlist=[self.visible, self.explored],
             choicelist=[self.tiles["light"], self.tiles["dark"]],
-            default=tile_types.SHROUD
+            default=tile_types.SHROUD,
         )
 
         for entity in self.entities:
