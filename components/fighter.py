6273782e059941026c983e8c1e20dc4cbbf41084
Gawain Hill <gawain.hill@googlemail.com>
2024-06-05 09:59:43 +0100

    feat: actors can take damage and die

--- components/fighter.py
+++ components/fighter.py
@@ -1,6 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from components.base_component import BaseComponent
 
+if TYPE_CHECKING:
+    from entity import Actor
+
 class Fighter(BaseComponent):
+    entity: Actor
+
     def __init__(self, health: int, defence: int, power: int) -> None:
         self.max_health = health
         self._health = health
@@ -17,3 +25,19 @@ class Fighter(BaseComponent):
         Sets health to a value between zero and max_health
         """
         self._health = max(0, min(value, self.max_health))
+        if self._health <= 0 and self.entity.ai:
+            self.die()
+
+    def die(self) -> None:
+        if self.engine.player is self.entity:
+            death_message = "You died!"
+        else:
+            death_message = f"{self.entity.name} is dead!"
+
+        self.entity.char = "%"
+        self.entity.color = (191, 0, 0)
+        self.entity.blocks_movement = False
+        self.entity.ai = None
+        self.entity.name = f"remains of {self.entity.name}"
+
+        print(death_message)
